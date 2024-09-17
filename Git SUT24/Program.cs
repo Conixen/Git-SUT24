7e6d63854ac1bd7b8c99510882835a0ae1fec10c
Conixen <leon.json57@gmail.com>
2024-09-17 11:55:46 +0200

    meny val lEon

--- Git SUT24/Program.cs
+++ Git SUT24/Program.cs
@@ -9,6 +9,45 @@
             string userNick = Console.ReadLine();
             Console.WriteLine($"Greetings {userNick}");
 
+
+            Console.WriteLine("Choose a number");
+
+            Console.WriteLine("1. Skriv ett ord " +
+                "\n2. Skriv ett number " +
+                "\n3.Avsluta program");
+            string userNum = Console.ReadLine();
+            int nr = Convert.ToInt32(userNum);
+
+
+            switch (nr)
+            {
+
+                case 1:
+                    Console.WriteLine("Skriv ett ord");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Skriv ett tal");
+                    
+                    break;
+
+                case 3:
+
+                    break;
+
+                default:
+                    Console.WriteLine("Vad håller du på med");
+                    break;
+
+
+
+
+
+
+
+            }
+           
+
         }
     }
 }
